ba496a2e37d96b6b21c22d44c67ae96022abf6a6
Zoe Kirsh <zoekirsh@gmail.com>
2021-02-12 12:03:33 -0800

    green walllll

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,29 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  // const renderMovie = (moviedata) => {
+  //   moviedata.map(movie => {
+  //     <h2>
+  //   })
+  // }
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {/* {renderMovie(movies)} */}
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <h3>Name: {movie.title}</h3>
+            <p>Time: {movie.time}</p>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,47 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+
+  const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'pink',
+    textDecoration: 'none',
+    color: 'magenta',
+  }
+
+
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink 
+        to="/"
+        exact
+        style={link}
+        activeStyle={{
+          textShadow: 'magenta' 
+        }}>Home</NavLink>
+      <NavLink 
+        to="/movies"
+        exact
+        style={link}
+        activeStyle={{
+          textShadow: 'magenta' 
+        }}>Movies</NavLink>
+      <NavLink  
+        to="/directors"
+        exact
+        style={link}
+        activeStyle={{
+          textShadow: 'magenta' 
+        }}>Directors</NavLink>
+      <NavLink  
+        to="/actors"
+        exact
+        style={link}
+        activeStyle={{
+          textShadow: 'magenta' 
+        }}>Actors</NavLink>
     </div>
   );
 };
